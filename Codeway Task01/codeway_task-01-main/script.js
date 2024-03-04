c845e4fce7274013360c23fa02dacc08d469ae8e
Sreevishnu <131434978+Bllister@users.noreply.github.com>
2024-03-04 22:58:43 +0530

    Add files via upload

---
+++ Codeway Task01/codeway_task-01-main/script.js
@@ -0,0 +1,15 @@
+function showsidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hidesidebar(){
+   const sidebar = document.querySelector('.sidebar')
+   sidebar.style.display = 'none'
+}
+
+
+
+
+
+
